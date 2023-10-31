55040c10d5be665ecd05645bf646a348ebaf3c85
shusaku009 <shusaku009@gmail.com>
2023-10-31 09:13:14 +0900

    clean: リファクタリング

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
-import { Page1DetailA } from "../Page1DetailA";
-import { Page1DetailB } from "../Page1DetailB";
 import { Page2 } from "../Page2";
+import { page1Routes } from "./Page1Routes";
 
 
 export const Router = () => {
@@ -15,18 +13,17 @@ export const Router = () => {
       <Route 
       path="/page1" 
       render={({ match: { url } }) => (
-          <Switch>
-          <Route exact path={url}>
-            <Page1 />
-          </Route>
-          <Route path={`${url}/detailA`}>
-            <Page1DetailA />
-          </Route>
-          <Route path={`${url}/detailB`}>
-            <Page1DetailB />
-          </Route>
-            <Page1 />
-          </Switch>
+        <Switch>
+          {page1Routes.map((route) => (
+            <Route 
+              key={route.path} 
+              exact={route.exact}
+              path={`${url}${route.path}`}
+              >
+              {route.children}
+            </Route>
+          ))}
+        </Switch>
         )}
       />
       <Route path="/page2">
